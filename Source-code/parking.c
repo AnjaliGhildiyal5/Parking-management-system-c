c8e62dd276d0067f0731207dccd4b1fc6500aced
AnjaliGhildiyal5 <ghildiyalanjali1985@gmail.com>
2026-07-19 17:27:05 +0530

    Update parking.c

--- Source-code/parking.c
+++ Source-code/parking.c
@@ -3,3 +3,24 @@
 
 #include "parking.h"
 #include "queue.h"
+
+Vehicle vehicles[MAX_VEHICLES];
+
+int vehicleCount = 0;
+
+int nextParkingID = 1001;
+
+double totalRevenue = 0;
+
+int totalVehiclesServed = 0;
+int nextReceipt = 1;
+
+Queue freeSlots;
+
+void initializeParking()
+{
+    initializeQueue(&freeSlots);
+
+    loadRecords();
+}
+
